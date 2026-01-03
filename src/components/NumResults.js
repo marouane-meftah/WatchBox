6bc1ef04ae11154ac94f3e87603502afa3495b66
Marouane Meftah <marwan_meftah@hotmail.fr>
2026-01-03 22:04:30 +0100

    Adding a funny thing that i will remove later

--- src/components/NumResults.js
+++ src/components/NumResults.js
@@ -1,10 +1,14 @@
 export default function NumResults({ watched }) {
     return (
-      <p className="num-results">
-        <strong>{watched.length}</strong>
-        {watched.length === 0 || watched.length === 1
-          ? " movie watched"
-          : " movies watched"}
-      </p>
+      // <p className="num-results">
+      //   <strong>{watched.length}</strong>
+      //   {watched.length === 0 || watched.length === 1
+      //     ? " movie watched"
+      //     : " movies watched"}
+      // </p>
+  
+    <div className="wlcm">
+     Welcome, Princess Aya ❤️
+    </div>
     );
   }
