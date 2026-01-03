cbce856b84cb145e295ec517fb9dd48f4f45cea3
Marouane Meftah <marwan_meftah@hotmail.fr>
2026-01-03 20:06:39 +0100

    Fix responsive

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -34,7 +34,7 @@ export default function MovieDetails({
       async function fetchMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
@@ -95,7 +95,7 @@ export default function MovieDetails({
                 <>
                   <StarRating
                     maxRating={10}
-                    size={24}
+                    size={18}
                     onSetRating={setUserRating}
                     defaultRating={userRating}
                   />
